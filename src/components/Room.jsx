e673308ec1fd7da7a77ab547e7181caf8d508688
YoheiHayamizu <hayamizu0111@gmail.com>
2023-04-11 00:47:10 -0400

    update

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -29,6 +29,7 @@ export const Room = () => {
                 response = await fetch(url, {
                     method: 'POST',
                     headers: {
+                        'Access-Control-Allow-Origin': '*',
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({ "token": token })
