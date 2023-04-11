ff88935f178f41e16bc88df99a0ca3a5e5ce7adf
YoheiHayamizu <hayamizu0111@gmail.com>
2023-04-10 23:45:54 -0400

    update url

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
-    // const url = "https://rest-dlg-server.herokuapp.com/token"
-    const url = "http://localhost:3500/token"
+    const url = "https://rest-dlg-server.herokuapp.com/token"
+    // const url = "http://localhost:3500/token"
 
     const selectInstance = () => {
         // Randomlly select a house id
