80552e75fe136e1ed7a0c540ad1ac3960f7a8e65
YoheiHayamizu <hayamizu0111@gmail.com>
2023-04-23 23:42:08 -0400

    update jsx files and json files

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,73 @@
-import { React } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { Component } from "react";
 
 import "./App.css";
 
 
-import { Home } from "./components/Home";
-import { Room } from "./components/Room";
-import { Page404 } from "./components/Page404";
+// import { Home } from "./components/Home";
+// import { Room } from "./components/Room";
+// import { Page404 } from "./components/Page404";
+import HomePage from "/app/rest_chat/src/components/HomePage";
+import ChatPage from "/app/rest_chat/src/components/ChatPage";
 
 
-function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            token: '',
+            session: null,
+            isChatStart: false,
+        };
+        this.handleStart = this.handleStart.bind(this);
+        this.handleEnd = this.handleEnd.bind(this);
+    }
+
+    componentDidMount() {
+        const token = localStorage.getItem('token');
+        const session = localStorage.getItem('session');
+
+        if (session) {
+            this.setState({
+                token: token,
+                session: JSON.parse(session),
+                isChatStart: true,
+            });
+            // console.log("Reloaded: ", token, session);
+        }
+    }
+
+    handleStart(token, session) {
+        localStorage.setItem('token', token);
+        localStorage.setItem('session', JSON.stringify(session));
+        console.log("Start a chat: ", token, session);
+        this.setState({ token: token, session: session, isChatStart: true });
+    }
+
+    handleEnd() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('session');
+        this.setState({ token: '', session: null, isChatStart: false });
+    }
+
+
+    render() {
+        const { token, session, isChatStart } = this.state;
+
+        return (
+            <div className="App" >
                 <div className="App-header">
                     <header>
                         <h1>REst Chat</h1>
                     </header>
                 </div>
-                <Routes>
-                    <Route key="/" path="/" exact={true} element={<Home />} />
-                    <Route key="/room" path="/room" exact={true} element={<Room />}>
-                        <Route key=":id" path=":id" element={<Room />} />
-                    </Route>
-                    <Route key="*" path="*" exact={false} element={<Page404 />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+                {!isChatStart ? (
+                    <HomePage handleStart={this.handleStart} />
+                ) : (
+                    <ChatPage token={token} session={session} handleEnd={this.handleEnd} />
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
